9eaa43363bcc8aa82e59e1f109190ef4a44193c0
Piotr Mińkowski <piotr_minkowski@wp.pl>
2019-04-21 01:00:23 +0200

    adding swagger

--- src/main/java/pl/piomin/services/main/MainApp.java
+++ src/main/java/pl/piomin/services/main/MainApp.java
@@ -1,7 +1,18 @@
 package pl.piomin.services.main;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Sample Application",
+                version = "1.0",
+                description = "Sample API",
+                contact = @Contact(url = "https://piotrminkowski.wordpress.com", name = "Piotr Mińkowski", email = "piotr.minkowski@gmail.com")
+        )
+)
 public class MainApp {
 
     public static void main(String[] args) {
