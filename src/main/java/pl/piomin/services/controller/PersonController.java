2650a5172b2480c6269b3b49d5f4aa3493939eec
piomin <piotr.minkowski@gmail.com>
2024-05-06 23:47:52 +0200

    update to the new micronaut 4 version

--- src/main/java/pl/piomin/services/controller/PersonController.java
+++ src/main/java/pl/piomin/services/controller/PersonController.java
@@ -8,8 +8,8 @@ import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.validation.Validated;
 import pl.piomin.services.model.Person;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
